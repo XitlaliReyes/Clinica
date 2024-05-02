1303e93ffeba61873f6ceb57ce6fbeb130038b70
uriel <uriel.ao@live.com>
2024-05-01 18:56:54 -0600

    Fix: Cambiado nombre de carpeta

--- medicalCenter/src/app/shared/doctor.service.spec.ts
+++ medicalCenter/src/app/shared/doctor.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SharedService } from './shared.service';
+import { DoctorService } from './doctor.service';
 
-describe('SharedService', () => {
-  let service: SharedService;
+describe('DoctorService', () => {
+  let service: DoctorService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(SharedService);
+    service = TestBed.inject(DoctorService);
   });
 
   it('should be created', () => {
