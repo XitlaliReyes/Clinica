12cd2dd610c5142cd7af6e51d9023b993b1f2e97
JuanDluna <pablo.dluna@hotmail.com>
2024-05-02 23:07:14 -0600

    Filtro por fecha
    Ahora en las citas marcadas se ve filtrado por citas pasadas y citas posteriores (nombre una reata yo)

--- medicalCenter/src/app/citas/reporte-citas/reporte-citas.component.ts
+++ medicalCenter/src/app/citas/reporte-citas/reporte-citas.component.ts
@@ -12,8 +12,20 @@ import { Cita } from '../cita.modle';
 export class ReporteCitasComponent {
 
     citas!: Cita[];
+    fechaActual!: Date;
 
     constructor(private citasService: CitasService) {
       this.citas = this.citasService.getCitas();
+      this.fechaActual = new Date();
     }
+
+    fechaPosterior(fechaCita: Date): any {
+      // Por alguna razón desconocida aunque se pase como tipo Date, se tienen que convertir a Date
+      // Para poder usar "getTime()" y hacer la comparacion correcta de las citas a tiempo correcto
+      // y asi filtrarlas de las citas que ya pasaron
+      let fechaCitaDate = new Date(fechaCita);
+      return fechaCitaDate.getTime() > this.fechaActual.getTime();
+    }
+
+
 }
