0b1f3df446bea7e07739cc68a2d5415383d1669f
Xitlali Reyes <al262973@edu.uaa.mx>
2024-05-03 18:21:52 -0600

    Links y rutas
    Rutas para ver mas sobre el doctor (perfil) al dar clic en su imagen o botón desde doctores

--- medicalCenter/src/app/app.routes.ts
+++ medicalCenter/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { DoctoresComponent } from './doctores/doctores.component';
 import { AltaCitaComponent } from './citas/alta-cita/alta-cita.component';
 import { ReporteCitasComponent } from './citas/reporte-citas/reporte-citas.component';
 import { SearchComponent } from './search/search.component';
+import { UndoctorComponent } from './undoctor/undoctor.component';
 
 
 export const routes: Routes = [
@@ -16,7 +17,7 @@ export const routes: Routes = [
     {path: 'alta-cita',component: AltaCitaComponent},
     {path: 'alta-cita/:nombre',component: AltaCitaComponent},
     {path: 'reporte-cita',component: ReporteCitasComponent},
-    // {path: 'doctor/:id', component: DoctoresComponent},
+    {path: 'doctor/:id', component: UndoctorComponent},
     {path: 'buscador/:nombred', component: SearchComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
