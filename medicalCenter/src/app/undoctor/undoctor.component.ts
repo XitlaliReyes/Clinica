e8e3b6682c932559f964d428691f8a0519bcefb9
Xitlali Reyes <al262973@edu.uaa.mx>
2024-05-06 13:30:40 -0600

    Diseño detalle
    Detalles finales

--- medicalCenter/src/app/undoctor/undoctor.component.ts
+++ medicalCenter/src/app/undoctor/undoctor.component.ts
@@ -3,18 +3,21 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Doctor } from '../doctor';
 import { DoctorService } from '../shared/doctor.service';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-undoctor',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, TableModule],
   templateUrl: './undoctor.component.html',
   styleUrl: './undoctor.component.css'
 })
 export class UndoctorComponent {
 
   @Input() doctor!: Doctor;
-  constructor(public doctorService: DoctorService, public activatedRoute: ActivatedRoute){
+  constructor(public doctorService: DoctorService, public activatedRoute: ActivatedRoute, private config: PrimeNGConfig){
     this.activatedRoute.params.subscribe(params => {  
       this.doctor = doctorService.getUnDoctor(params['id'])
     })
