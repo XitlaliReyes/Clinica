385020ada226bde7f02d76eccbe70f2666b31948
Xitlali Reyes <al262973@edu.uaa.mx>
2024-05-06 21:40:10 -0600

    Correcciones
    OPG

--- medicalCenter/src/app/acerca/miembros.interface.ts
+++ medicalCenter/src/app/acerca/miembros.interface.ts
@@ -1,11 +1,4 @@
-export interface Miembros {
-    nombre: string;
-    rol: string;
-    especialidad: string;
-    telefono: string;
-    imagenUrl:string;
 
-}
 export interface MiembrosDesarrollo {
     nombre:string;
     imagenUrl:string;
